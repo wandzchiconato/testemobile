1f12363869ac31bdb3f6c41659e1287ceb8b77aa
Wandz <Wandz@DESKTOP-U5ISC2T>
2019-05-21 11:53:46 -0300

    update teste Motorola

--- TesteMobile/src/test/java/mobile/mobile_motorola.java
+++ TesteMobile/src/test/java/mobile/mobile_motorola.java
@@ -2,7 +2,7 @@ package mobile;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-//import junit.framework.TestCase;
+import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,34 +12,34 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class mobile_motorola {
 
-  private AndroidDriver<MobileElement> driver;
+  private AndroidDriver driver;
 
   @Before
   public void setUp() throws MalformedURLException {
     DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    desiredCapabilities.setCapability("udid", "0036795549");
+    desiredCapabilities.setCapability("deviceName", "Moto_G__4_");
     desiredCapabilities.setCapability("platformName", "Android");
-    desiredCapabilities.setCapability("deviceName", "emulator-5554");
     desiredCapabilities.setCapability("automationName", "uiautomator2");
-    desiredCapabilities.setCapability("appPackage", "com.sec.android.app.popupcalculator");
-    desiredCapabilities.setCapability("appActivity", "com.sec.android.app.popupcalculator.Calculator");
+    desiredCapabilities.setCapability("appPackage", "com.google.android.calculator");
+    desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+    desiredCapabilities.setCapability("noReset", "true");
 
     URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 
-    driver = new AndroidDriver<MobileElement>(remoteUrl, desiredCapabilities);
+    driver = new AndroidDriver(remoteUrl, desiredCapabilities);
   }
 
   @Test
   public void sampleTest() {
-    MobileElement el1 = (MobileElement) driver.findElementById("com.sec.android.app.popupcalculator:id/bt_01");
+    MobileElement el1 = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_1");
     el1.click();
-    MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("Mais");
+    MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("multiplicar");
     el2.click();
-    MobileElement el3 = (MobileElement) driver.findElementById("com.sec.android.app.popupcalculator:id/bt_02");
+    MobileElement el3 = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_8");
     el3.click();
-    MobileElement el4 = (MobileElement) driver.findElementByAccessibilityId("Igual");
+    MobileElement el4 = (MobileElement) driver.findElementByAccessibilityId("igual");
     el4.click();
-    MobileElement el5 = (MobileElement) driver.findElementByAccessibilityId("Campo de entrada da Calculadora");
-    el5.click();
   }
 
   @After
